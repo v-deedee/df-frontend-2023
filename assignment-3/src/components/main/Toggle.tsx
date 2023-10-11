2649ff3d71592dbf0367faf09de68d1fdd9c78c8
DeeDee <21020277@vnu.edu.vn>
2023-10-11 15:15:28 +0700

    Save light-dark mode in localStorage

--- assignment-3/src/components/main/Toggle.tsx
+++ assignment-3/src/components/main/Toggle.tsx
@@ -1,7 +1,17 @@
 import { useEffect, useState } from 'react';
 
 const Toggle = () => {
-  const [isDark, setIsDark] = useState(false);
+  const [isDark, setIsDark] = useState(() => {
+    let state = '';
+    try {
+      const localData = localStorage.getItem('theme');
+      state = localData === null ? 'light' : JSON.parse(localData);
+    } catch (error) {
+      console.error('error');
+    }
+
+    return state === 'dark';
+  });
 
   const [mode, setMode] = useState('Light');
 
@@ -9,9 +19,11 @@ const Toggle = () => {
     if (isDark) {
       document.querySelector('body')?.setAttribute('data-theme', 'dark');
       setMode('Dark');
+      localStorage.setItem('theme', JSON.stringify('dark'));
     } else {
       document.querySelector('body')?.setAttribute('data-theme', 'light');
       setMode('Light');
+      localStorage.setItem('theme', JSON.stringify('light'));
     }
   }
 
@@ -20,7 +32,7 @@ const Toggle = () => {
   return (
     <div id='toggle-box'>
       <label className='switch' htmlFor='toggle-btn'>
-        <input type='checkbox' id='toggle-btn' checked={isDark} onClick={() => setIsDark(!isDark)}/>
+        <input type='checkbox' id='toggle-btn' checked={isDark} onChange={() => setIsDark(!isDark)}/>
         <span className='slider' />
       </label>
       <p>{mode} mode</p>
